f53caefd6fe3669ca60604a5d79e2a96d1ce5f34
George Skoumas <skoumasgeorge@gmail.com>
2024-07-17 15:23:35 +0300

    fixed typing in networks

--- SurrealGAN/networks.py
+++ SurrealGAN/networks.py
@@ -1,3 +1,5 @@
+from typing import Any, List
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -19,37 +21,37 @@ __status__ = "Development"
 ###############################################################################
 
 
-def weights_init(m):  # type: ignore
+def weights_init(m: nn.Module) -> None:
     classname = m.__class__.__name__
     if classname.find("Linear") != -1:
         m.weight.data.normal_(0, 0.1)
 
 
-def define_Linear_Mapping(nROI, nPattern):  # type: ignore
-    netG = LMappingGenerator(nPattern, nROI)
+def define_Linear_Mapping(nROI: Any, nPattern: int) -> nn.Module:
+    netG = LMappingGenerator(nPattern, nROI)  # type: ignore
     netG.apply(weights_init)
     return netG
 
 
-def define_Linear_Discriminator(nROI):  # type: ignore
+def define_Linear_Discriminator(nROI: Any) -> nn.Module:
     netD = LDiscriminator(nROI)
     netD.apply(weights_init)
     return netD
 
 
-def define_Linear_Reconstruction(nROI, nPattern):  # type: ignore
+def define_Linear_Reconstruction(nROI: Any, nPattern: int) -> nn.Module:
     netC = LEncoder(nPattern, nROI)
     netC.apply(weights_init)
     return netC
 
 
-def define_Linear_Decomposer(nROI, nPattern):  # type: ignore
+def define_Linear_Decomposer(nROI: Any, nPattern: int) -> nn.Module:
     netD = LDecompose(nPattern, nROI)
     netD.apply(weights_init)
     return netD
 
 
-def define_Latent_Corr(ndim):  # type: ignore
+def define_Latent_Corr(ndim: int) -> nn.Module:
     netL = LLatentCorr(ndim)
     netL.apply(weights_init)
     return netL
@@ -61,11 +63,11 @@ def define_Latent_Corr(ndim):  # type: ignore
 
 
 class LMappingGenerator(nn.Module):
-    def __init__(self, nPattern, nROI, product_layer=Sub_Adder):  # type: ignore
+    def __init__(self, nPattern: int, nROI: Any, product_layer: Sub_Adder) -> None:
         super(LMappingGenerator, self).__init__()
         model = []
 
-        def block(in_layer, out_layer, normalize=False):  # type: ignore
+        def block(in_layer: Any, out_layer: Any, normalize: bool = False) -> Any:
             layers = [nn.Linear(in_layer, out_layer, bias=False)]
             if normalize:
                 layers.append(nn.BatchNorm1d(out_layer, 0.8))
@@ -78,48 +80,44 @@ class LMappingGenerator(nn.Module):
         # model.append(nn.Linear(nROI, nROI,bias=False))
         self.model = TwoInputSequential(*model)
 
-    def forward(self, input_x, input_z):  # type: ignore
+    def forward(self, input_x: torch.Tensor, input_z: torch.Tensor) -> torch.Tensor:
         return self.model(input_x, input_z)
 
 
 class LEncoder(nn.Module):
-    def __init__(self, nPattern, nROI):  # type: ignore
+    def __init__(self, nPattern: int, nROI: Any) -> None:
         super(LEncoder, self).__init__()
         model = []
         self.nPattern = nPattern
 
-        def block(in_layer, out_layer, normalize=True):  # type: ignore
-            layers = []
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-            layers.append(nn.Linear(in_layer, out_layer))
+        def block(in_layer: Any, out_layer: Any) -> Any:
+            layers = [nn.LeakyReLU(0.2, inplace=True), nn.Linear(in_layer, out_layer)]
             return layers
 
         model.append(nn.Linear(nROI, int(nROI / 2)))
         model += block(int(nROI / 2), int(nROI / 4)) + block(int(nROI / 4), nPattern)
         self.model = nn.Sequential(*model)
 
-    def forward(self, input_y):  # type: ignore
+    def forward(self, input_y: torch.Tensor) -> torch.Tensor:
         zt = self.model(input_y)
         return zt
 
 
 class LDecompose(nn.Module):
-    def __init__(self, nPattern, nROI):  # type: ignore
+    def __init__(self, nPattern: int, nROI: Any) -> None:
         super(LDecompose, self).__init__()
         model = []
         self.nPattern = nPattern
         self.nROI = nROI
 
-        def block(in_layer, out_layer):  # type: ignore
-            layers = []
-            layers.append(nn.LeakyReLU(0.2, inplace=False))
-            layers.append(nn.Linear(in_layer, out_layer))
+        def block(in_layer: Any, out_layer: Any) -> Any:
+            layers = [nn.LeakyReLU(0.2, inplace=False), nn.Linear(in_layer, out_layer)]
             return layers
 
         model += block(int(nROI), int(nPattern * nROI))
         self.model = nn.Sequential(*model)
 
-    def forward(self, input_y):  # type: ignore
+    def forward(self, input_y: torch.Tensor) -> List[torch.Tensor]:
         z_decompose = self.model(input_y)
         return [
             z_decompose[:, self.nROI * i : self.nROI * (i + 1)]
@@ -128,7 +126,7 @@ class LDecompose(nn.Module):
 
 
 class LDiscriminator(nn.Module):
-    def __init__(self, nROI):  # type: ignore
+    def __init__(self, nROI: Any) -> None:
         super(LDiscriminator, self).__init__()
         self.model = nn.Sequential(
             nn.Linear(nROI, int(nROI / 2), bias=True),
@@ -138,13 +136,13 @@ class LDiscriminator(nn.Module):
             nn.Linear(int(nROI / 4), 2, bias=True),
         )
 
-    def forward(self, input_y):  # type: ignore
+    def forward(self, input_y: torch.Tensor) -> torch.Tensor:
         pred = self.model(input_y)
         return pred
 
 
 class LLatentCorr(nn.Module):
-    def __init__(self, ndim):  # type: ignore
+    def __init__(self, ndim: int) -> None:
         super(LLatentCorr, self).__init__()
         self.model = nn.Sequential(
             nn.Linear(1, ndim, bias=False),
@@ -152,6 +150,6 @@ class LLatentCorr(nn.Module):
             nn.Linear(ndim, ndim, bias=False),
         )
 
-    def forward(self):  # type: ignore
+    def forward(self) -> torch.Tensor:
         pred = F.tanh(self.model(nn.Parameter(torch.tensor([1.0]))))
         return pred
